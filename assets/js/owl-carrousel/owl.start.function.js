abf18faeae66c4584429eb1557f5e204d57ac492
Diogoasc7 <diogodsc1456@gmail.com>
2025-06-04 19:57:11 -0300

    libs incluidas

---
+++ assets/js/owl-carrousel/owl.start.function.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
